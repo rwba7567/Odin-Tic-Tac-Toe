ec3b4f6f9f34fe5772e8a20cfcd5d4f9290422ab
rwba7567 <rwba7567@gmail.com>
2022-06-07 22:17:49 +0800

    Script menuStart btn

--- script.js
+++ script.js
@@ -15,7 +15,16 @@ const game = () => {
         board[pos] = sign;
     };
 
+
+
     return {turn};
 }
 
+//menu Start Button script
+let menuBtn = document.querySelector("#startMenu > div > button");
+let textBoard = document.querySelector("#textOutput");
 
+menuBtn.addEventListener("click", function(){
+    document.querySelector("#startMenu").style.height = 0;
+    textBoard.innerText = "Player 1 (X) Move"
+});
