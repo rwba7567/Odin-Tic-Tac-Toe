7cb63e576ff5969bd7ea77533b8ffbe773358cbf
rwba7567 <rwba7567@gmail.com>
2022-06-07 15:49:05 +0800

    Add Game Module

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ const player = (name,sign) => {
 
 const adam = player('ryan', "X");
 
+//game module
+const game = () => {
+    let board = ["", "", "",
+                 "", "", "",
+                 "", "", ""];
+
+    const turn = (sign,pos) => {
+        board[pos] = sign;
+    };
+
+    return {turn};
+}
+
+
